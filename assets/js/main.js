ce19fc625fcd3d2fff5797b300eb0f9aa6a2ccac
divppc <okal.ihor@gmail.com>
2017-10-20 22:05:09 +0300

    Header added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-	// body...
+	
+	//mobile toggle function start
+
+  $(".mobile-menu").on("click", function () {
+    $(this).toggleClass('active');
+    $('.menu-line').toggleClass('active');
+  });
+
+  // function mobileToggle(e) {
+  //   if ($(e).hasClass("active") == true) {
+  //     $(e).animate({maxHeight: "0"}, 300).removeClass("active");
+  //   } else {
+  //     $(e).animate({maxHeight: "100vh"}, 300).addClass("active");
+  //   }
+  // };  
+  //mobile toggle function end
 });
